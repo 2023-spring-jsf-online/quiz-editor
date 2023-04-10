1c8a6cf29fcbebd38897fbc7df44579b3faf0462
Ascher <atownsend3@madisoncollege.edu>
2023-04-10 16:53:43 -0500

    Defining and shaping data

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -8,19 +8,21 @@ export class QuizService {
   constructor() { }
 
   loadQuizzes = () => {
-    const quizzesFromWeb: any[] = [];
-      { name: 'Quiz 1'
+    const quizzesFromWeb: any[] = [
+      { name: 'Quiz 1',
         questions: [
           {name: 'Question 1'},
           {name: 'Question 2'}
         ]
-      }
+      },
       {
-        name: 'Quiz 2'
+        name: 'Quiz 2',
         questions: [
 
         ]
       }
+      ];
+
     return quizzesFromWeb;
   };
 }
