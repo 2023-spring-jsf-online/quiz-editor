90044d646985472b64a1e8f67376a0f122ca94b1
Austin Brown <austinnbrown95@gmail.com>
2023-05-02 17:39:12 -0500

    deleted quizzes badge working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -158,4 +158,12 @@ export class AppComponent implements OnInit {
     this.selectedQuiz = undefined;
   };
 
+  getDeletedQuizzes = () => {
+    return this.quizzes.filter(x => x.markedForDelete);
+  };
+
+  get deletedQuizCount() {
+    return this.getDeletedQuizzes().length;
+  };
+
 }
