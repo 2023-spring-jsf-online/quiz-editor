cb3a272f81642a74c85bb7000412f083ce94c483
Ascher <atownsend3@madisoncollege.edu>
2023-05-08 13:24:08 -0500

    Save API works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService, QuizFromWeb } from './quiz.service';
+import { QuizService, QuizFromWeb, ShapeForSavingEditedQuizzes, ShapeForSavingNewQuizzes } from './quiz.service';
 import { trigger, transition, animate, keyframes, style } from "@angular/animations";
 
 interface QuizDisplay {
@@ -224,5 +224,29 @@ export class AppComponent implements OnInit {
   detailsFromLeftAnimationDone = () => {
     this.detailsFromLeftAnimationState = "leftPosition";
   };
+
+  saveQuizzes = async() => {
+    try {
+      const newQuizzes: ShapeForSavingNewQuizzes[] = this.getAddedQuizzes().map(x => ({
+        quizName: x.quizName,
+        quizQuestions: x.quizQuestions.map(y => y.questionName)
+
+      }));
+
+      const editedQuizzes: ShapeForSavingEditedQuizzes[] = this.getEditedQuizzes().map(x => ({
+        quiz: x.quizName,
+        questions: x.quizQuestions.map(y => ({question: y.questionName}))
+      }));
+
+      const numberOfUpdatedQuizzes = await this.quizSvc.saveQuizzes(
+        editedQuizzes,
+        newQuizzes
+      );
+      console.log("numberofUdatedQuizzes" , numberOfUpdatedQuizzes);
+      
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
 }
